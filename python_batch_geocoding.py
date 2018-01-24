a5cd0892b1ef0156211143d09a2ae6e63aa2d5ec
crazybusy <35750263+crazybusy@users.noreply.github.com>
2018-01-24 21:17:54 +0530

    merge my fork

--- python_batch_geocoding.py
+++ python_batch_geocoding.py
@@ -1,19 +1,14 @@
 """
 Python script for batch geocoding of addresses using the Google Geocoding API.
-
 This script allows for massive lists of addresses to be geocoded for free by pausing when the 
 geocoder hits the free rate limit set by Google (2500 per day).  If you have an API key for paid
 geocoding from Google, set it in the API key section.
-
 Addresses for geocoding can be specified in a list of strings "addresses". In this script, addresses
 come from a csv file with a column "Address". Adjust the code to your own requirements as needed.
-
 After every 500 successul geocode operations, a temporary file with results is recorded in case of 
 script failure / loss of connection later.
-
 Addresses and data are held in memory, so this script may need to be adjusted to process files line
 by line if you are processing millions of entries.
-
 Shane Lynn
 5th November 2016
 """
@@ -22,13 +17,7 @@ import pandas as pd
 import requests
 import logging
 import time
-
-logger = logging.getLogger("root")
-logger.setLevel(logging.DEBUG)
-# create console handler
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
+import os
 
 #------------------ CONFIGURATION -------------------------------
 
@@ -38,17 +27,34 @@ logger.addHandler(ch)
 # With a "Google Maps Geocoding API" key from https://console.developers.google.com/apis/, 
 # the daily limit will be 2500, but at a much faster rate.
 # Example: API_KEY = 'AIzaSyC9azed9tLdjpZNjg2_kVePWvMIBq154eA'
-API_KEY = None
+API_KEY = 'AIzaSyD0H_G1JKCgklUtvDFVcdoMtto3ooyalZ8'
 # Backoff time sets how many minutes to wait between google pings when your API limit is hit
 BACKOFF_TIME = 30
 # Set your output file name here.
-output_filename = 'data/output-2015.csv'
+output_filename = 'data/output.csv'
 # Set your input file here
-input_filename = "data/PPR-2015.csv"
+input_filename = "data/input.csv"
 # Specify the column name in your input data that contains addresses here
 address_column_name = "Address"
+index_label = "SNo"
+#Write temp file after this many records
+temp_file_dump_after = 500
+#Write counters after this many records
+counts_show_after = 100
 # Return Full Google Results? If True, full JSON results from Google are included in output
 RETURN_FULL_RESULTS = False
+#The level of the logger
+log_level = logging.DEBUG
+
+#------------------ Logging -------------------------------------
+
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+# create console handler
+ch = logging.StreamHandler()
+ch.setLevel(log_level)
+logger.addHandler(ch)
+
 
 #------------------ DATA LOADING --------------------------------
 
@@ -62,13 +68,22 @@ if address_column_name not in data.columns:
 # Make a big list of all of the addresses to be processed.
 addresses = data[address_column_name].tolist()
 
-# **** DEMO DATA / IRELAND SPECIFIC! ****
-# We know that these addresses are in Ireland, and there's a column for county, so add this for accuracy. 
-# (remove this line / alter for your own dataset)
-addresses = (data[address_column_name] + ',' + data['County'] + ',Ireland').tolist()
-
-
 #------------------	FUNCTION DEFINITIONS ------------------------
+'''
+Use Utility function from websearch to lookup an address
+The function returns the number of web results to give confidence number based on number of results
+It also returns any spelling suggestion google has for the address
+'''
+#Note that the results are a little different because of personalized and local search results.
+
+def lookup_web(address, output):
+        from websearch import search_item
+        web_answer = search_item(address)
+
+        output["correction"] = web_answer.get("correction")
+        output['number_of_web_results'] = web_answer.get("formattedTotalResults")
+
+        return output
 
 def get_google_results(address, api_key=None, return_full_response=False):
     """
@@ -160,19 +175,34 @@ for address in addresses:
             # Note that the results might be empty / non-ok - log this
             if geocode_result['status'] != 'OK':
                 logger.warning("Error geocoding {}: {}".format(address, geocode_result['status']))
-            logger.debug("Geocoded: {}: {}".format(address, geocode_result['status']))
+                logger.info("Looking up google web custom search for full address")
+                geocode_result= lookup_web(address, geocode_result)
+                logger.debug("{} results found".format(geocode_result.get("number_of_web_results")))
+            else:
+                logger.debug("Geocoded: {}: {}".format(address, geocode_result['status']))
             results.append(geocode_result)           
             geocoded = True
 
     # Print status every 100 addresses
-    if len(results) % 100 == 0:
+    if len(results) % counts_show_after == 0:
     	logger.info("Completed {} of {} address".format(len(results), len(addresses)))
             
     # Every 500 addresses, save progress to file(in case of a failure so you have something!)
-    if len(results) % 500 == 0:
-        pd.DataFrame(results).to_csv("{}_bak".format(output_filename))
+    if len(results) % temp_file_dump_after  == 0:
+        logger.debug("Writing Temp File")
+        pd.DataFrame(results).to_csv("{}_bak".format(output_filename), index_label=index_label)
 
 # All done
 logger.info("Finished geocoding all addresses")
 # Write the full results to csv using the pandas library.
-pd.DataFrame(results).to_csv(output_filename, encoding='utf8')
+logger.info("Writing Output File")
+pd.DataFrame(results).to_csv(output_filename, encoding='utf8', index_label=index_label)
+
+#Cleanup if needed and if actual file has a modified timestamp after the temp file
+logger.debug("Cleanup Temp File")
+if os.path.isfile("{}_bak".format(output_filename)) and os.path.isfile(output_filename) :
+        if(os.path.getmtime(output_filename) > os.path.getmtime("{}_bak".format(output_filename))):
+                os.remove("{}_bak".format(output_filename))
+        else:
+                logger.debug("It seems temp file is newer than output file. Output file needs checking")
+        
